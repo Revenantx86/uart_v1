152b5e2eede56a712cbdfb386ef5ab171b05317d
Revenantx86 <69749268+Revenantx86@users.noreply.github.com>
2024-01-09 10:05:53 -0500

    initial top module commit

---
+++ uart.v
@@ -0,0 +1,33 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * Refik Yalcin wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a coffe or ginger ale in return.
+ * ----------------------------------------------------------------------------
+ *
+ * File Name: uart.v
+ *
+ * Project: UART Communication module 
+ *
+ * Module Name: UART Top module 
+ *
+ * Description: <Universal asyncronus send/receive communicaiton protocol module implemented using verilog for gate level synthesis 
+ *
+ * Functional Description:
+ * This file contains the top module construction of the all uart protocol modules
+ *
+ *
+ * Revision History:
+ * Rev 1.0 - <Date>, <Your Name> - Initial release
+ *
+ * Additional Notes:
+ * - Note 1: Baud controller created
+ *
+ */
+module UART ();
+
+
+
+
+endmodule
