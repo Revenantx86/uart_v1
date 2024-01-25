02192a63ef5b7cea6bedd798dd6d709b11db8a4b
Revenantx86 <69749268+Revenantx86@users.noreply.github.com>
2024-01-24 22:16:30 -0500

    config update

--- config.mk
+++ config.mk
@@ -8,6 +8,8 @@ export VERILOG_FILES = ./user_design/$(DESIGN_NICKNAME)/uart.v \
 
 export SDC_FILE      = ./user_design/$(DESIGN_NICKNAME)/constraint.sdc
 
-export CORE_UTILIZATION = 40
+export CORE_UTILIZATION = 70
+export CORE_ASPECT_RATIO = 2
+export FP_PDN_CORE_RING = 1
 
 export TNS_END_PERCENT = 100
